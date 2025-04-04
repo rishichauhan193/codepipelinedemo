4bfea60e374240b81dd7b75a44f4ce6cf3e97834
Rishi Chauhan <rishichauhan1419@gmail.com>
2025-04-04 11:49:17 +0530

    Change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> YoYo :-)
+          Edit <code>src/App.js</code> Rishi Chauhan
         </p>
         <a
           className="App-link"
