6bbd11fbc28a49cfe207a7c4ddf32cd4b1655698
Rishi Chauhan <rishichauhan1419@gmail.com>
2025-04-04 15:14:22 +0530

    Changing Three Files

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,32 @@
+# #!/bin/bash
+# set -e
+
+# echo ">>> Switching to project directory"
+# cd /home/ec2-user/server
+
+# echo ">>> Ensuring PM2 is running the backend"
+# pm2 delete Frontend || true
+# pm2 start server.js --name Frontend
+
+# echo ">>> Deployment script completed successfully."
+
+
+
+
+
 #!/bin/bash
+
 set -e
 
-echo ">>> Switching to project directory"
-cd /home/ec2-user/server
+echo ">>> Pulling latest image from ECR"
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 418295711710.dkr.ecr.ap-south-1.amazonaws.com
 
-echo ">>> Ensuring PM2 is running the backend"
-pm2 delete Frontend || true
-pm2 start server.js --name Frontend
+IMAGE_URI="418295711710.dkr.ecr.ap-south-1.amazonaws.com/my-project-image:latest"
 
-echo ">>> Deployment script completed successfully."
+echo ">>> Stopping old container (if running)"
+docker rm -f my-app || true
+
+echo ">>> Running new container"
+docker run -d --name my-app -p 3009:3009 $IMAGE_URI
+
+echo ">>> Deployment finished!"
