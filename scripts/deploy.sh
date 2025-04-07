191de8b419ab7ac1537cb20ef511f306af39ca69
Rishi Chauhan <rishichauhan1419@gmail.com>
2025-04-07 10:02:33 +0530

    Changes in deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,18 +16,17 @@
 
 #!/bin/bash
 
+# Stop the script if any command fails
 set -e
 
-echo ">>> Pulling latest image from ECR"
+echo "Logging in to Amazon ECR..."
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 418295711710.dkr.ecr.ap-south-1.amazonaws.com
 
-
+# Define your image path from ECR
 IMAGE_URI="418295711710.dkr.ecr.ap-south-1.amazonaws.com/my-project-image:latest"
 
-echo ">>> Stopping old container (if running)"
-docker rm -f my-app || true
-
-echo ">>> Running new container"
+echo "🚀 Running the new container..."
 docker run -d --name my-app -p 3009:3009 $IMAGE_URI
 
-echo ">>> Deployment finished!"
+echo "Deployment complete! Your app should now be running on port 3009."
+
