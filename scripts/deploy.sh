cc335141a06674ab6ffb6d10763790bf0035a129
Rishi Chauhan <rishichauhan1419@gmail.com>
2025-04-07 10:26:05 +0530

    Change in buildspec

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -25,7 +25,7 @@ aws ecr get-login-password --region ap-south-1 | docker login --username AWS --p
 # Define your image path from ECR
 IMAGE_URI="418295711710.dkr.ecr.ap-south-1.amazonaws.com/my-project-image:latest"
 
-echo "🚀 Running the new container..."
+echo "Running the new container..."
 docker run -d --name my-app -p 3009:3009 $IMAGE_URI
 
 echo "Deployment complete! Your app should now be running on port 3009."
